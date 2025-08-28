80c139065fde7ed82531b6c907714fe151cf7ce6
Ken <bassettkennethj@gmail.com>
2025-08-27 22:39:45 -0400

    Added spiral drawing

--- drawing.py
+++ drawing.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import math
 import random
 
 class Drawing(ABC):
@@ -39,7 +40,7 @@ class parabola(Drawing):
     def __init__(self, canvas_width, canvas_height):
         super().__init__(canvas_width, canvas_height)
         self.direction = random.choice(['vertical', 'horizontal'])
-        self.coeff = random.random() * 2 - 1  # random number between -1 and 1
+        self.coeff = random.random() * 0.1 - 0.05  # random number between -1 and 1
     
     def _draw(self, canvas):
         # start from the middle and draw outwards in both directions at the same time
@@ -75,5 +76,29 @@ class parabola(Drawing):
         canvas[y2, x2] = self.color
 
 
+class spiral(Drawing):
+    def __init__(self, canvas_width, canvas_height):
+        super().__init__(canvas_width, canvas_height)
+        self.delta_radius = random.random() * 2
+        self.delta_angle = random.random() * math.pi # 0-180 degrees
+        self.max_iterations = random.random() * max(canvas_height, canvas_width) / self.delta_angle
+
+    def _draw(self, canvas):
+        r = self.delta_radius * self.i
+        a = self.delta_angle * self.i
+        x = round(self.x + r * math.cos(a))
+        y = round(self.y + r * math.sin(a))
+        if x >= canvas.shape[1] or x < 0:
+            self.complete = True
+            return
+        if y >= canvas.shape[0] or y < 0:
+            self.complete = True
+            return
+        canvas[y, x] = self.color
+
+        if self.i >= self.max_iterations:
+            self.complete = True
+
+
 # put your drawing class in this list if you want it to appear
-drawing_choices = [dot, parabola]
+drawing_choices = [dot, parabola, spiral]
